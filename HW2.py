131401082b6b17a87753e7bf0cd480a9177b019d
BedirT <tapkan@ualberta.ca>
2021-11-29 11:18:55 -0700

    hw2 added

---
+++ HW2.py
@@ -0,0 +1,73 @@
+# Exercise 1: Print First 10 natural numbers using a for loop
+
+# Exercise 2: Print the following pattern
+# 1 
+# 1 2 
+# 1 2 3 
+# 1 2 3 4 
+# 1 2 3 4 5
+
+# Exercise 3: Calculate the sum of all numbers from 1 to a given number
+
+# Exercise 4: Write a program to print multiplication table of a given number
+
+# Exercise 5: Display numbers from a list using loop
+
+# Exercise 6: Count the total number of digits in a number
+
+# Exercise 7: Print the following pattern
+# 5 4 3 2 1 
+# 4 3 2 1 
+# 3 2 1 
+# 2 1 
+# 1
+
+# Exercise 8: Print list in reverse order using a loop
+
+# Exercise 9: Display numbers from -10 to -1 using for loop
+
+# Exercise 10: Create a list with 10 of your favorite bands, display them
+# with the most favorite starting from 1, in descending order.
+
+# Exercise 11: Write a program to display all prime numbers within a range
+
+# Exercise 12: Display Fibonacci series up to 10 terms
+
+# Exercise 13: Find the factorial of a given number
+
+# Exercise 14: Reverse a given integer number
+
+# Exercise 15: Use a loop to display elements from a given list present at odd index positions
+
+# Exercise 16: Calculate the cube of all numbers from 1 to a given number
+# Write a program to rint the cube of all numbers from 1 to a given number
+
+# Exercise 17: Find the sum of the series upto n terms
+# Write a program to calculate the sum of series up to n term. For example, 
+# if n =5 the series will become 2 + 22 + 222 + 2222 + 22222 = 24690
+
+# Exercise 18: Print the following pattern
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * * 
+# * * * * 
+# * * * 
+# * * 
+# *
+
+# Exercise 19: Print the following pattern
+#         * 
+#       * * *
+#     * * * * * 
+#   * * * * * * *
+# * * * * * * * * * 
+#   * * * * * * *
+#     * * * * * 
+#       * * *
+#         *
+
+# Exercise 20: Using a list for questions, ask user personal
+# information and store it in another list. Each question should be
+# in the form '1-Please enter your name: ' and so on.
